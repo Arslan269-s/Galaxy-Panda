dd40b15d62513d99636fdc72e1d4adadbc912e1e
Asadullah Khan <asadmuhammad427@gmail.com>
2021-09-23 13:51:46 +0500

    pull changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,80 +4,7 @@ import { Connection, Transaction,clusterApiUrl } from "@solana/web3.js";
 // import  splToken from '@solana/spl-token';
 
 export const Header = () => {
-<<<<<<< HEAD
-    return (
-        // <header className="transparent">
-		// 	<div className="container">
-		// 		<div className="row">
-		// 			<div className="col-md-12">
-		// 				<div className="de-flex sm-pt10">
-		// 					<div className="de-flex-col">
-		// 						<div className="de-flex-col">
 
-		// 							<div id="logo">
-		// 								<a href="index.html">
-		// 									<img alt="" src={logoImg}	
-		// 										style={{width: "90px", height: "90px", display:"inline-block", marginRight: "-5px", position: "relative", top: "2px", marginRight: "4px", marginBottom: "5px"}} />
-		// 									<span className="pixel-font"
-		// 										style={{color: "#FFF",fontSize: "36px",display: "inline-block",verticalAlign: "middle",letterSpacing: "1px",fontFamily: "bangers", color: "#00FFAB",}}>galaxy
-		// 										panda</span>
-		// 								</a>
-		// 							</div>
-
-		// 						</div>
-		// 					</div>
-		// 					<div className="de-flex-col header-col-mid">
-
-		// 						<ul id="mainmenu">
-		// 							<li>
-		// 								<a href="index.html">Home<span></span></a>
-		// 							</li>
-		// 							<li>
-		// 								<a href="roadmap.html">Roadmap<span></span></a>
-		// 							</li>
-		// 							<li>
-		// 								<a href="rankings.html">Rarity Ranking<span></span></a>
-		// 							</li>
-		// 							<li>
-		// 								<a href="attributes.html">Attributes<span></span></a>
-		// 							</li>
-		// 							<li>
-		// 								<a href="index.html#section-stats">Mint Stats<span></span></a>
-		// 							</li>
-		// 							<li>
-		// 								<a href="index.html#section-faq">FAQ<span></span></a>
-		// 							</li>
-		// 						</ul>
-		// 						<div className="menu_side_area">
-		// 							<a href="#" className="btn-main" style={{display: "inline-block",							backgroundColor:"#550D8F",}}>Connect Wallet</a>
-		// 							<span className="menu-btn"></span>
-		// 						</div>
-		// 					</div>
-		// 				</div>
-		// 			</div>
-		// 		</div>
-		// 	</div>
-		// </header>
-		<header className="transparent">
-			<div className="container">
-				<div className="row">
-					<div className="col-md-12">
-						<div className="de-flex sm-pt10">
-							<div className="de-flex-col">
-								<div className="de-flex-col">
-
-									<div id="logo">
-										<a href="index.html">
-											<div className="header-logo-img"style={{width: "90px", height: "90px", display:"inline-block", marginRight: "-5px", position: "relative", top: "2px", marginRight: "4px", marginBottom: "5px"}}>
-										<img alt="" src={logoImg}	
-											 />
-											</div>
-											<span className="pixel-font"
-													style={{color: "#FFF",fontSize: "36px",display: "inline-block",verticalAlign: "middle",letterSpacing: "1px",fontFamily: "bangers", color: "#00FFAB",}}>galaxy
-												panda</span>
-										</a>
-									</div>
-=======
   const [account, setAccount] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -86,7 +13,7 @@ export const Header = () => {
     // window.solana.on("connect", () => {
     //   let address = window.solana.publicKey.toString();
     //   setAccount(address);
-    // });
+    // })
     setLoading(false);
   }, []);
   async function connect(e) {
@@ -102,7 +29,6 @@ export const Header = () => {
       console.log(`Phantom is not Installed`);
     }
   }
->>>>>>> 086df1dc853cc65c489192f04dbfe72566997353
 
   async function disconnect(e) {
     e.preventDefault();
@@ -110,50 +36,7 @@ export const Header = () => {
     setAccount(null);
   }
 
-<<<<<<< HEAD
-								<ul id="mainmenu">
-									<li>
-										<a href="index.html">Home<span></span></a>
-									</li>
-									<li>
-										<a href="roadmap.html">Roadmap</a>
-									</li>
 
-									<li className="dropdown"><a href="#">NFT </a>
-										<ul className="dropdown-menu">
-											<li><a href="rankings.html">Rarity Ranking</a></li>
-											<li><a href="attributes.html">Attributes</a></li>
-											<li><a href="index.html#section-stats">Mint Stats</a></li>
-										</ul>
-									</li>
-									
-
-									<li>
-										<a href="attributes.html">Token<span></span></a>
-									</li>
-									<li>
-										<a href="attributes.html">Game<span></span></a>
-									</li>
-									
-
-									<li>
-										<a href="index.html#section-faq">FAQ<span></span></a>
-									</li>
-								</ul>
-								<div className="menu_side_area">
-									<a href="#" className="btn-main"
-										style={{display: "inline-block",							backgroundColor:"#550D8F",}}>Connect Wallet</a>
-									<span id="menu-btn"></span>
-								</div>
-						</div>
-					</div>
-				</div>
-			</div>
-	</div>
-	</header>
-    )
-}
-=======
   return (
     <header className="transparent">
       {loading ? (
@@ -265,4 +148,3 @@ export const Header = () => {
     </header>
   );
 };
->>>>>>> 086df1dc853cc65c489192f04dbfe72566997353
